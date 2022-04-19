54895c22faf39ab2f17b165d667069ef2669d900
dafo <daniel.forniessoria@tomtom.com>
2022-04-19 13:30:37 +0200

    chore(scaffolding): adding basic dependencies
    GitOrigin-RevId: 638d37408ab124c2debda31f7494020333eef6ba

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 120,
+    trailingComma: "none",
+    arrowParens: "always",
+    overrides: [
+        {
+            files: "*.scss",
+            options: {
+                tabWidth: 2,
+                printWidth: 80
+            }
+        }
+    ]
+};
