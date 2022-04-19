b735e94c3ad2745d88b3b7670b834aa2183df6a5
dafo <daniel.forniessoria@tomtom.com>
2022-04-19 18:13:02 +0200

    chore(scaffolding): adding basic dependencies, mock files
    GitOrigin-RevId: df52def1545c52b683c74d06a3a001e3c4b4af9a

---
+++ core/src/types/Location.ts
@@ -0,0 +1,80 @@
+export type Address = {
+    /**
+     * The building number on the street.
+     */
+    streetNumber?: string;
+
+    /**
+     * The street name.
+     */
+    streetName?: string;
+
+    /**
+     * Sub / Super City
+     */
+    municipalitySubdivision?: string;
+
+    /**
+     * City / Town
+     */
+    municipality?: string;
+
+    /**
+     * County
+     */
+    countrySecondarySubdivision?: string;
+
+    /**
+     * Named Area
+     */
+    countryTertiarySubdivision?: string;
+
+    /**
+     * State or Province
+     */
+    countrySubdivision?: string;
+
+    /**
+     * Postal Code / Zip Code
+     */
+    postalCode?: string;
+
+    /**
+     * Extended postal code (availability dependent on region)
+     */
+    extendedPostalCode?: string;
+
+    /**
+     * Country (Note: This is a two-letter code, not a country name.)
+     */
+    countryCode?: string;
+
+    /**
+     * Country name
+     */
+    country?: string;
+
+    /**
+     * ISO alpha-3 country code
+     */
+    countryCodeISO3?: string;
+
+    /**
+     * An address line formatted according to formatting
+     * rules of a result's country of origin, or in case
+     * of countries its full country name.
+     */
+    freeformAddress?: string;
+
+    /**
+     * A full name of a first level of country administrative hierarchy.
+     * This field appears only in case countrySubdivision is presented in an abbreviated form.
+     * Supported only for USA, Canada and Great Britain.
+     */
+    countrySubdivisionName?: string;
+
+    /**
+     * An address component which represents the name of a geographic area or locality that groups a number of addressable objects for addressing purposes, without being an administrative unit.
+     */
+    localName?: string;
+};
