e33e9fbfd47e82c3183602efece543c5778cecb0
dafo <daniel.forniessoria@tomtom.com>
2022-04-20 17:34:15 +0200

    chore(scaffolding): adding basic dependencies
    GitOrigin-RevId: c34053a489127f9ca05297cbb07720c1da939518

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,31 +1,47 @@
+import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
+// We use rollup-plugin-typescript2 instead of @rollup/plugin-typescript
+// because the latter is not compatible with rollup-plugin-web-worker-loader.
+// More info: https://github.com/darionco/rollup-plugin-web-worker-loader/issues/57
+import { terser } from "rollup-plugin-terser";
 
 export default () => {
+    const plugins = [
+        // has to be before typescript plugin
+        nodeResolve({ browser: true }),
+        typescript(),
+        commonjs(),
+        terser()
+    ];
+
     return [
         {
             input: "core/src/index.ts",
             output: {
                 file: "core/dist/core.prod.js",
-                format: "es"
+                format: "es",
+                sourcemap: true
             },
-            plugins: [typescript()]
+            plugins
         },
         {
             input: "services/src/index.ts",
             output: {
                 file: "services/dist/services.prod.js",
-                format: "es"
+                format: "es",
+                sourcemap: true
             },
-            plugins: [typescript()]
+            plugins
         },
         {
             input: "map/src/index.ts",
             output: {
                 file: "map/dist/map.prod.js",
-                format: "es"
+                format: "es",
+                sourcemap: true
             },
-            plugins: [typescript()]
+            plugins
         }
     ];
 };
