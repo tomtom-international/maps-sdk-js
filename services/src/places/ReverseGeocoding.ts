146924b27ce525c9c625bc65e1f82292733f8abe
dafo <daniel.forniessoria@tomtom.com>
2022-04-21 17:40:19 +0200

    chore(scaffolding): basic test mock
    GitOrigin-RevId: d6569b7473e2e1bc48b76b8101800b6ecb680759

--- services/src/places/ReverseGeocoding.ts
+++ services/src/places/ReverseGeocoding.ts
@@ -3,9 +3,16 @@ import { Address } from "core/src";
 import { ReverseGeocodingOptions } from "./ReverseGeocodingOptions";
 
 export const reverseGeocode = async (options: ReverseGeocodingOptions): Promise<Address> =>
-    axios.get("/52.33499,5.72884.json", {
-        baseURL: "https://api.tomtom.com/search/2/reverseGeocode",
-        params: {
-            key: "XVxgvGPnXxuAHlFcKu1mBTGupVwhVlOE"
-        }
+    new Promise((resolve, reject) => {
+        axios
+            .get("/52.33499,5.72884.json", {
+                baseURL: "https://api.tomtom.com/search/2/reverseGeocode",
+                params: {
+                    key: "XVxgvGPnXxuAHlFcKu1mBTGupVwhVlOE"
+                }
+            })
+            .then(
+                (resolved) => resolve(resolved.data?.addresses[0]?.address),
+                (rejected) => reject(rejected)
+            );
     });
