7ada0d4a5c5a4a1f64bf2c499de2852abcaea43c
Oleksii Popov <Oleksii.Popov@tomtom.com>
2022-06-02 14:14:44 +0200

    feat(actions): ability to release and remove npm branch packages
    GitOrigin-RevId: d6213c589833a30f01f62734026c26ed7586c649

---
+++ scripts/switch-version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+export NODE_OPTIONS=--max_old_space_size=4096
+
+# Get tag name from the first argument
+tagName=$1;
+
+# Temporary file
+tmp=$(mktemp);
+
+previousVersion=$(cat package.json | jq -r .version);
+echo "Current package.json version is $previousVersion";
+
+# Get latest package version for given tag
+pkg_v=$(npm dist-tag ls @anw/go-sdk-js | sed -n 's/^'"${tagName}"': //p');
+
+if [ "$pkg_v" = "" ];
+then
+    pkg_v=$previousVersion;
+fi
+
+# Check current resolved version
+echo "Current pre-release version is $pkg_v";
+
+# Replace base version with resolved one, so the release script could calculate the next version
+jq --arg v "$pkg_v" '.version = $v' package.json > "$tmp" && mv "$tmp" package.json
+jq --arg v "$pkg_v" '.version = $v' package-lock.json > "$tmp" && mv "$tmp" package-lock.json
