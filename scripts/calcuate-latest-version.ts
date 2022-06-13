871ec3c7b86a2ef834ea71ef9f9dc9c92b8d807d
Oleksii Popov <Oleksii.Popov@tomtom.com>
2022-06-13 16:25:05 +0200

    chore(cleaning): remove commented out code
    GitOrigin-RevId: efefbe0b7a0b7a2e887e30fb3f93baec76b9fd8b

--- scripts/calcuate-latest-version.ts
+++ scripts/calcuate-latest-version.ts
@@ -1,8 +1,6 @@
 const [, , ...args] = process.argv;
 const [localVersion, remoteVersion] = args;
 
-//console.log({alias, localVersion, remoteVersion});
-
 const resolveResult = (result: string) => {
     process.stdout.write(result, "utf-8");
     process.exit();
@@ -14,9 +12,6 @@ if (!remoteVersion) {
 
 const [l_maj, l_min, l_patch] = (localVersion.match(/(\d+)\.(\d+)\.(\d+)/)?.slice(1, 4) ?? []).map(Number);
 const [r_maj, r_min, r_patch] = (remoteVersion.match(/(\d+)\.(\d+)\.(\d+)/)?.slice(1, 4) ?? []).map(Number);
-
-//console.log({l_maj, l_min, l_patch});
-//console.log({r_maj, r_min, r_patch});
 
 if (l_maj > r_maj) {
     resolveResult(localVersion);
