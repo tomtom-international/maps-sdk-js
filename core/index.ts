e74c31336c8c33ca76ff7aff0001b54948f0b0fd
kulesevi <klunt je vecan>
2022-07-14 10:59:02 +0200

    chore(documentation): working on setting up documentation for go-sdk-js
    GitOrigin-RevId: 80bc1d970dca5e7232883db19d8047d66bed6ebc

--- core/index.ts
+++ core/index.ts
@@ -5,5 +5,6 @@
 export * from "./src/config/GlobalConfig";
 export * from "./src/types/Geometry";
 export * from "./src/types/Location";
+export * from "./src/types/LocationDataSources";
 export * from "./src/types/View";
 export * from "./src/util/LngLat";
--- services/index.ts
+++ services/index.ts
@@ -2,4 +2,14 @@
  * This module contains functions that enable connection to TomTom services.
  * @module
  */
+export * from "./src/revgeo/RequestBuilder";
+export * from "./src/revgeo/ResponseParser";
 export * from "./src/revgeo/ReverseGeocoding";
+export * from "./src/revgeo/ReverseGeocodingParams";
+export * from "./src/revgeo/ReverseGeocodingTemplate";
+
+export * from "./src/shared/Arrays";
+export * from "./src/shared/Fetch";
+export * from "./src/shared/Geometry";
+export * from "./src/shared/ServiceTemplate";
+export * from "./src/shared/ServiceTypes";
