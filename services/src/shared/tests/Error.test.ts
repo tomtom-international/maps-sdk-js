8acad4a52acfa110fa67ed2f36cb37f27b3edeba
Guilherme Scaldelai <guilherme.scaldelai@tomtom.com>
2022-08-31 11:02:17 +0200

    refactor(error-handler): removing try/catch from testing.
    GitOrigin-RevId: 124f351c6968dccb2d577dd18f10eaef26c05524

--- services/src/shared/tests/Error.test.ts
+++ services/src/shared/tests/Error.test.ts
@@ -2,30 +2,16 @@ import { SDKError } from "../Errors";
 
 describe("SDKError tests", () => {
     it("should return object when catch an Error instance", async () => {
-        let error;
-        try {
-            throw new Error("Testing");
-        } catch (e) {
-            error = e;
-        } finally {
-            const sdkError = new SDKError(error);
-            expect(sdkError.message).toEqual("Testing");
-            expect(sdkError.service).toEqual(undefined);
-            expect(sdkError.stack).not.toBeNull();
-        }
+        const sdkError = new SDKError(new Error("Testing"));
+        expect(sdkError.message).toEqual("Testing");
+        expect(sdkError.service).toEqual(undefined);
+        expect(sdkError.stack).not.toBeNull();
     });
 
     it("should show custom message and service when passed as arguments", async () => {
-        let error;
-        try {
-            throw new Error("Testing");
-        } catch (e) {
-            error = e;
-        } finally {
-            const sdkError = new SDKError(error, "Routing", "This is a new message");
-            expect(sdkError.message).toEqual("This is a new message");
-            expect(sdkError.service).toEqual("Routing");
-            expect(sdkError.stack).not.toBeNull();
-        }
+        const sdkError = new SDKError(new Error("Testing"), "Routing", "This is a new message");
+        expect(sdkError.message).toEqual("This is a new message");
+        expect(sdkError.service).toEqual("Routing");
+        expect(sdkError.stack).not.toBeNull();
     });
 });
