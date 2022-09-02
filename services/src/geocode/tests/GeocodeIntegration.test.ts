c2cb5a5b37ef77f2318a33b8710feab7945e0884
shanbhag <soorajrajkumar.shanbhag@tomtom.com>
2022-09-02 17:45:25 +0530

    tests(geocoding): move test data to json files & addressing impacts in tests
    GitOrigin-RevId: 52e0062e0eee5600da63735b04af12f565844073

--- services/src/geocode/tests/GeocodeIntegration.test.ts
+++ services/src/geocode/tests/GeocodeIntegration.test.ts
@@ -1,9 +1,10 @@
-import { singleResultExample, multiResultExample, customParserExample } from "./GeocodingIntegration.data";
+import parsedResponses from "./GeocodingIntegration.data.json";
 import geocode from "../Geocoding";
 import { GeocodingResponse } from "../types/GeocodingResponse";
 import { GeocodingResponseAPI } from "../types/APITypes";
 import { putIntegrationTestsAPIKey } from "../../shared/tests/IntegrationTestUtils";
 import { SDKError } from "../../shared/Errors";
+import { GeocodingParams } from "../types/GeocodingParams";
 
 describe("Geocoding test without API key", () => {
     test("Geocoding test without API key", async () => {
@@ -19,15 +20,13 @@ describe("Geocoding test without API key", () => {
 describe("Geocoding integration tests", () => {
     beforeAll(() => putIntegrationTestsAPIKey());
 
-    test("Geocoding single results", async () => {
-        const result = await geocode({ query: "teakhout zaandam" });
-        expect(result).toMatchObject(singleResultExample);
-    });
-
-    test("Geocoding multi results", async () => {
-        const result = await geocode({ query: "teakhout" });
-        expect(result).toMatchObject(multiResultExample);
-    });
+    test.each(parsedResponses)(
+        `'%s`,
+        // @ts-ignore
+        async (_name: string, params: GeocodingParams, sdkResponse: never) => {
+            expect(await geocode(params)).toMatchObject(sdkResponse);
+        }
+    );
 
     test("Geocoding with all parameters sent", async () => {
         const result = await geocode({
@@ -64,6 +63,48 @@ describe("Geocoding integration tests", () => {
     });
 
     test("Geocoding with template response override to get only the first raw result and summary", async () => {
+        const customParserExample = {
+            result: {
+                type: "Street",
+                score: 2.1169600487,
+                matchConfidence: {
+                    score: 1
+                },
+                address: {
+                    streetName: "Teakhout",
+                    municipalitySubdivision: "Zaandam",
+                    municipality: "Zaanstad",
+                    countrySubdivision: "Noord-Holland",
+                    countryCode: "NL",
+                    country: "Nederland",
+                    countryCodeISO3: "NLD",
+                    freeformAddress: "Teakhout, Zaanstad",
+                    localName: "Zaanstad"
+                },
+                position: {
+                    lat: 52.44131,
+                    lon: 4.8093
+                },
+                viewport: {
+                    topLeftPoint: {
+                        lat: 52.44179,
+                        lon: 4.80845
+                    },
+                    btmRightPoint: {
+                        lat: 52.44009,
+                        lon: 4.81063
+                    }
+                }
+            },
+            summary: {
+                query: "teakhout",
+                queryType: "NON_NEAR",
+                numResults: 4,
+                offset: 0,
+                totalResults: 4,
+                fuzzyLevel: 1
+            }
+        };
         const result = await geocode(
             { query: "teakhout" },
             {
--- services/src/geocode/tests/ResponseParser.test.ts
+++ services/src/geocode/tests/ResponseParser.test.ts
@@ -1,19 +1,12 @@
-import {
-    apiResponse0,
-    sdkResponse0,
-    apiResponse1,
-    sdkResponse1,
-    apiResponse2,
-    sdkResponse2
-} from "./ResponseParser.data";
+import apiAndParsedResponses from "./ResponseParser.data.json";
 import { parseGeocodingResponse } from "../ResponseParser";
 
 describe("Geocode response parsing tests", () => {
-    test("parsing actual API responses", () => {
-        expect(parseGeocodingResponse(apiResponse0)).toStrictEqual(sdkResponse0);
-        expect(parseGeocodingResponse(apiResponse1)).toStrictEqual(sdkResponse1);
-    });
-    test("mocked addressRanges", () => {
-        expect(parseGeocodingResponse(apiResponse2)).toStrictEqual(sdkResponse2);
-    });
+    test.each(apiAndParsedResponses)(
+        `'%s`,
+        // @ts-ignore
+        async (_name: string, apiResponse: never, sdkResponse: never) => {
+            expect(parseGeocodingResponse(apiResponse)).toStrictEqual(sdkResponse);
+        }
+    );
 });
