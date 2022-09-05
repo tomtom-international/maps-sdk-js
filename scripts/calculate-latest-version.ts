8ed8068e5c3c919ce7185200cb66c479906e4593
Oleksii Popov <Oleksii.Popov@tomtom.com>
2022-09-05 17:33:30 +0200

    fix(versioning): regex should be resistant to backtracking
    GitOrigin-RevId: 5347101c0898f1bb021313b095e802f61c57d532

--- scripts/calculate-latest-version.ts
+++ scripts/calculate-latest-version.ts
@@ -1,5 +1,6 @@
 const [, , ...args] = process.argv;
 const [localVersion, remoteVersion, repoVersion] = args;
+const versionRegex = /^(\d+)\.(\d+)\.(\d+)([a-z0-9-.]){0,100}$/gi;
 
 const resolveResult = (result: string) => {
     process.stdout.write(result, "utf-8");
@@ -12,8 +13,8 @@ if (!remoteVersion) {
     // new alias or re-creation of the old one
     version = localVersion;
 } else {
-    const [l_maj, l_min, l_patch] = (localVersion.match(/(\d+)\.(\d+)\.(\d+)/)?.slice(1, 4) ?? []).map(Number);
-    const [r_maj, r_min, r_patch] = (remoteVersion.match(/(\d+)\.(\d+)\.(\d+)/)?.slice(1, 4) ?? []).map(Number);
+    const [l_maj, l_min, l_patch] = (localVersion.match(versionRegex)?.slice(1, 4) ?? []).map(Number);
+    const [r_maj, r_min, r_patch] = (remoteVersion.match(versionRegex)?.slice(1, 4) ?? []).map(Number);
 
     if (l_maj > r_maj || l_maj === r_maj && l_min > r_min || l_min === r_min && l_patch >= r_patch) {
         version = localVersion;
@@ -22,8 +23,8 @@ if (!remoteVersion) {
     }
 }
 
-const [candidate_maj, candidate_min, candidate_patch] = (version.match(/(\d+)\.(\d+)\.(\d+)/)?.slice(1, 4) ?? []).map(Number);
-const [repo_maj, repo_min, repo_patch] = (repoVersion.match(/(\d+)\.(\d+)\.(\d+)/)?.slice(1, 4) ?? []).map(Number);
+const [candidate_maj, candidate_min, candidate_patch] = (version.match(versionRegex)?.slice(1, 4) ?? []).map(Number);
+const [repo_maj, repo_min, repo_patch] = (repoVersion.match(versionRegex)?.slice(1, 4) ?? []).map(Number);
 
 if (repo_maj === candidate_maj && repo_min === candidate_min && repo_patch === candidate_patch + 1) {
     // take the latest repo version. "1.2.3-my-old-version.4"
