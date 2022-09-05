76924c090d365a2418c6f525f302c39dd7b5ce53
Oleksii Popov <Oleksii.Popov@tomtom.com>
2022-09-05 16:37:29 +0200

    chore(core): fix eslint warnings
    GitOrigin-RevId: 05cc405b896a1cf3926f783a8097655c7273361d

--- core/src/types/Connector.ts
+++ core/src/types/Connector.ts
@@ -1,5 +1,19 @@
 export type Connector = {
-    connectorType: "StandardHouseholdCountrySpecific" | "IEC62196Type1" | "IEC62196Type1CCS" | "IEC62196Type2CableAttached" | "IEC62196Type2Outlet" | "IEC62196Type2CCS" | "IEC62196Type3" | "Chademo" | "GBT20234Part2" | "GBT20234Part3" | "IEC60309AC3PhaseRed" | "IEC60309AC1PhaseBlue" | "IEC60309DCWhite" | "Tesla";
+    connectorType:
+        | "StandardHouseholdCountrySpecific"
+        | "IEC62196Type1"
+        | "IEC62196Type1CCS"
+        | "IEC62196Type2CableAttached"
+        | "IEC62196Type2Outlet"
+        | "IEC62196Type2CCS"
+        | "IEC62196Type3"
+        | "Chademo"
+        | "GBT20234Part2"
+        | "GBT20234Part3"
+        | "IEC60309AC3PhaseRed"
+        | "IEC60309AC1PhaseBlue"
+        | "IEC60309DCWhite"
+        | "Tesla";
     ratedPowerKW: number;
     currentA: number;
     currentType: string;
