e2f5ad21072c257f640ee47fae789b99bed91e46
dafo <daniel.forniessoria@tomtom.com>
2022-09-05 16:22:35 +0200

    chore(security): hiding testing api key behind a safe environment variable
    GitOrigin-RevId: 7278f9b12fce7c78bd867cb6ab89b1d041cadfba

--- services/src/shared/tests/IntegrationTestUtils.ts
+++ services/src/shared/tests/IntegrationTestUtils.ts
@@ -2,6 +2,6 @@ import { GOSDKConfig } from "@anw/go-sdk-js/core";
 
 export const putIntegrationTestsAPIKey = () => {
     GOSDKConfig.instance.put({
-        apiKey: process.env.API_KEY_TESTS,
+        apiKey: process.env.API_KEY_TESTS
     });
 };
