e8fb2c4b308f9505b193fbadb1a4ab2cb4e5f1cf
Guilherme Scaldelai <guilherme.scaldelai@tomtom.com>
2022-09-05 12:11:28 +0200

    build(jest): Removing root config file
    GitOrigin-RevId: cb35aca956fddc8bc912fc43cb821e2b4a70c677

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    moduleDirectories: ['node_modules', 'core', 'map', 'services'],
-    moduleNameMapper: {
-        "@anw/go-sdk-js/core": "core"
-    }
-}
