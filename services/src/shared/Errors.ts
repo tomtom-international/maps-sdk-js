c9cd46243f0899bc8839d180304d861ef1ff6c17
Guilherme Scaldelai <guilherme.scaldelai@tomtom.com>
2022-09-07 15:58:15 +0200

    fix(error-handler): Add routing error handler
    GitOrigin-RevId: 70e04cc3aa7eb24b9bbffb06ba47f5c1be703189

--- services/src/shared/Errors.ts
+++ services/src/shared/Errors.ts
@@ -1,5 +1,5 @@
 import { AxiosError } from "axios";
-import { APIErrorCode, APIResponseError } from "./types/APIResponseErrorTypes";
+import { APIErrorCode, APIResponseError, RoutingAPIResponseError } from "./types/APIResponseErrorTypes";
 import { Services } from "./types/ServicesTypes";
 
 /**
@@ -26,22 +26,40 @@ export class SDKError extends Error {
     }
 
     private transformAxiosError() {
-        const { response, stack } = this.error as AxiosError<APIResponseError>;
+        const { response, stack } = this.error as AxiosError;
 
         this.status = response?.status;
         this.stack = stack;
 
-        /* We use as message what returns from API if any, otherwise we have our APIErrorCode as a fallback*/
-        if (response?.data?.error) {
-            this.message = response.data.error;
-            return;
+        /* A few services returns different error responses, we should be able to transform those responses based on the service */
+        if (this.service === "Routing") {
+            this.transformRouteAPIError();
+        } else {
+            this.transformDefaultAPIError();
         }
 
         /*
+         * We use as message what returns from API if any, otherwise we have our APIErrorCode as a fallback
          * Check if there is a status and if the status exists in the mapped API error types
          */
         if (this.status && APIErrorCode[this.status]) {
             this.message = APIErrorCode[this.status];
+        }
+    }
+
+    private transformDefaultAPIError() {
+        const { response } = this.error as AxiosError<APIResponseError>;
+
+        if (response?.data?.error) {
+            this.message = response.data.error;
+        }
+    }
+
+    private transformRouteAPIError() {
+        const { response } = this.error as AxiosError<RoutingAPIResponseError>;
+
+        if (response?.data.error.description) {
+            this.message = response?.data.error.description;
         }
     }
 }
--- services/src/shared/types/APIResponseErrorTypes.ts
+++ services/src/shared/types/APIResponseErrorTypes.ts
@@ -1,29 +1,52 @@
 export interface APIResponseError {
-    /**
+    /*
      * A human-readable description of the error code.
      */
     error: string;
-    /**
+    /*
      * HTTP error code.
      */
     httpStatusCode: number;
-    /**
+    /*
      * Detailed information about the error.
      */
     detailedError: {
-        /**
+        /*
          * One of the defined error codes.
          */
         code: string;
-        /**
+        /*
          * A human-readable representation of the error code.
          */
         message: string;
-        /**
+        /*
          * Target of the particular error.
          * Value: The name of the request parameter.
          */
         target: string;
+    };
+}
+
+export interface RoutingAPIResponseError {
+    /*
+     * 	The format version
+     */
+    formatVersion: string;
+    error: {
+        /*
+         * A human-readable representation of the error code.
+         */
+        description: string;
+    };
+    detailedError: {
+        /*
+         * A human-readable representation of the error code.
+         */
+        message: string;
+        /*
+         * One of the defined error codes.
+         */
+        code: string;
     };
 }
 
