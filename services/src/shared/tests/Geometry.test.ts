c5f0be9f513fa0bb3b29a7cee2721bbc884a739f
dafo <daniel.forniessoria@tomtom.com>
2022-09-07 16:15:15 +0200

    improvement(services): adding unit tests for bounding box mapping
    GitOrigin-RevId: 8fe2cb6ae2c9dcb702e04c407e7d65b78f33222e

--- services/src/shared/tests/Geometry.test.ts
+++ services/src/shared/tests/Geometry.test.ts
@@ -1,6 +1,28 @@
-import { polygonToBtmRightBBox, bboxToPolygon } from "../Geometry";
+import { polygonToBtmRightBBox, bboxToPolygon, apiToGeoJSONBBox } from "../Geometry";
 
 describe("Geometry tests", () => {
+    test("API to GeoJSON bounding boxes", () => {
+        expect(
+            apiToGeoJSONBBox({
+                topLeftPoint: {
+                    lat: 52.44179,
+                    lon: 4.80845
+                },
+                btmRightPoint: {
+                    lat: 52.44009,
+                    lon: 4.81063
+                }
+            })
+        ).toStrictEqual([4.80845, 52.44009, 4.81063, 52.44179]);
+
+        expect(
+            apiToGeoJSONBBox({
+                southWest: "52.44009,4.80845",
+                northEast: "52.44179,4.81063"
+            })
+        ).toStrictEqual([4.80845, 52.44009, 4.81063, 52.44179]);
+    });
+
     test("bbox to polygon then to bbox again", () => {
         expect(
             polygonToBtmRightBBox(
