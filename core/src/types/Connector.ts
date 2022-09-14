2573c26481b88a5d9b05b709dc6528ec76bdba7f
Oleksii Popov <Oleksii.Popov@tomtom.com>
2022-09-14 17:08:22 +0200

    refactor(core): extract and export ConnectorType type
    GitOrigin-RevId: 2b51e292d7b2e6f2af727db621ff194b9d8ce944

--- core/src/types/Connector.ts
+++ core/src/types/Connector.ts
@@ -1,19 +1,21 @@
+export type ConnectorType =
+    | "StandardHouseholdCountrySpecific"
+    | "IEC62196Type1"
+    | "IEC62196Type1CCS"
+    | "IEC62196Type2CableAttached"
+    | "IEC62196Type2Outlet"
+    | "IEC62196Type2CCS"
+    | "IEC62196Type3"
+    | "Chademo"
+    | "GBT20234Part2"
+    | "GBT20234Part3"
+    | "IEC60309AC3PhaseRed"
+    | "IEC60309AC1PhaseBlue"
+    | "IEC60309DCWhite"
+    | "Tesla";
+
 export type Connector = {
-    connectorType:
-        | "StandardHouseholdCountrySpecific"
-        | "IEC62196Type1"
-        | "IEC62196Type1CCS"
-        | "IEC62196Type2CableAttached"
-        | "IEC62196Type2Outlet"
-        | "IEC62196Type2CCS"
-        | "IEC62196Type3"
-        | "Chademo"
-        | "GBT20234Part2"
-        | "GBT20234Part3"
-        | "IEC60309AC3PhaseRed"
-        | "IEC60309AC1PhaseBlue"
-        | "IEC60309DCWhite"
-        | "Tesla";
+    connectorType: ConnectorType;
     ratedPowerKW: number;
     currentA: number;
     currentType: string;
