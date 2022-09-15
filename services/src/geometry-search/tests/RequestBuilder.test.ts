02b1c33626c55bc8183eabc8c5b82d7ac111da96
shanbhag <soorajrajkumar.shanbhag@tomtom.com>
2022-09-15 19:10:22 +0530

    tests(Geometry-Search): request builder tests
    GitOrigin-RevId: 2c7e96974ba8473ebce3bfa78e72493677ad0d04

---
+++ services/src/geometry-search/tests/RequestBuilder.test.ts
@@ -0,0 +1,14 @@
+import {buildGeometrySearchRequest} from "../RequestBuilder";
+import geometrySearchReqObjectsAndURLs from "./RequestBuilder.data.json";
+import {GeometrySearchRequest, SearchByGeometryPayloadAPI} from "../types";
+
+describe("Calculate Geometry Search request URL building tests", () => {
+    test.each(geometrySearchReqObjectsAndURLs)(
+        "'%s'",
+        // @ts-ignore
+        (name: string, params: GeometrySearchRequest, url: string, requestData: SearchByGeometryPayloadAPI) => {
+            expect(buildGeometrySearchRequest(params).url.href).toStrictEqual(url);
+            expect(JSON.stringify(buildGeometrySearchRequest(params).data)).toStrictEqual(JSON.stringify(requestData));
+        }
+    );
+});
