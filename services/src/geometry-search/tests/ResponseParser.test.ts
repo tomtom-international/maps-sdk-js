8eb5f0205f587c32c9c58d725efc17988b75b476
shanbhag <soorajrajkumar.shanbhag@tomtom.com>
2022-09-21 16:55:49 +0530

    test(geometry-search):response parser tests for geometry search
    GitOrigin-RevId: 42ccbf14a4683956d40a45d4ea4aaa8af5a6ab6e

---
+++ services/src/geometry-search/tests/ResponseParser.test.ts
@@ -0,0 +1,13 @@
+import {GeometrySearchResponse, GeometrySearchResponseAPI} from "../types";
+import apiAndParsedResponses from "../../geometry-search/tests/ResponseParser.data.json";
+import {parseGeometrySearchResponse} from "../ResponseParser";
+
+describe("Geometry Search response parser tests", () => {
+    test.each(apiAndParsedResponses)(
+        "'%s'",
+        // @ts-ignore
+        (_name: string, apiResponse: GeometrySearchResponseAPI, parsedResponse: GeometrySearchResponse) => {
+            expect(parseGeometrySearchResponse(apiResponse)).toStrictEqual(parsedResponse);
+        }
+    );
+});
