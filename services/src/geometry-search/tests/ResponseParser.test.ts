7608a277d732cb08e0e509c8373c5fae73c7a5dc
shanbhag <soorajrajkumar.shanbhag@tomtom.com>
2022-09-27 18:39:26 +0530

    tests(geometry-search): error scenarios [WIP]
    GitOrigin-RevId: 314ad8e310501ae27b5025d66f207823abedcacd

--- services/src/geometry-search/tests/ResponseParser.test.ts
+++ services/src/geometry-search/tests/ResponseParser.test.ts
@@ -1,6 +1,9 @@
 import {GeometrySearchResponse, GeometrySearchResponseAPI} from "../types";
 import apiAndParsedResponses from "../../geometry-search/tests/ResponseParser.data.json";
 import {parseGeometrySearchResponse} from "../ResponseParser";
+import errorResponses from "../../routing/tests/ResponseParserError.data.json";
+import {ErrorObjAPI, RoutingAPIResponseError} from "../../shared/types/APIResponseErrorTypes";
+import {routingResponseErrorParser} from "../../routing/RoutingResponseErrorParser";
 
 describe("Geometry Search response parser tests", () => {
     test.each(apiAndParsedResponses)(
@@ -8,6 +11,21 @@ describe("Geometry Search response parser tests", () => {
         // @ts-ignore
         (_name: string, apiResponse: GeometrySearchResponseAPI, parsedResponse: GeometrySearchResponse) => {
             expect(parseGeometrySearchResponse(apiResponse)).toStrictEqual(parsedResponse);
+        }
+    );
+});
+
+describe("Routing - error response parsing tests", () => {
+    test.each(errorResponses)(
+        "'%s'",
+        // @ts-ignore
+        // async (_name: string, apiResponseError: ErrorObjAPI<RoutingAPIResponseError>, expectedSDKError: string) => {
+        //     const sdkRoutingResponseError = routingResponseErrorParser(apiResponseError, "Routing");
+        //     expect(sdkRoutingResponseError).toMatchObject(expectedSDKError);
+        // }
+        async (_name: string, apiResponseError: ErrorObjAPI<RoutingAPIResponseError>) => {
+            const sdkRoutingResponseError = routingResponseErrorParser(apiResponseError, "GeometrySearch");
+            console.log(JSON.stringify(sdkRoutingResponseError));
         }
     );
 });
