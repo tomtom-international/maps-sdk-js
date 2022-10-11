547786a2635fef386600353086ed37ae60d217cf
Suleyman Ali <suleyman.ali@tomtom.com>
2022-10-11 11:21:57 +0200

    fix(versioning): select correct latest version of alias
    GitOrigin-RevId: cbe5dcec435c3ce29c75e90f158c037e5ebb1244

--- scripts/calculate-latest-version.ts
+++ scripts/calculate-latest-version.ts
@@ -1,6 +1,15 @@
 const [, , ...args] = process.argv;
-const [localVersion, remoteVersion, repoVersion] = args;
-const versionRegex = /^(\d+)\.(\d+)\.(\d+)([a-z0-9-.]){0,100}$/gi;
+const [localVersion, remoteVersion] = args;
+
+/* 
+ Given a version "1.2.3-ALIAS_NAME.0":
+ a match call returns ["1.2.3-ALIAS_NAME.0", "1", "2", "3", ...]
+ */
+const versionRegex = /^(\d+)\.(\d+)\.(\d+)/;
+
+const getVersionNumbers = (version: string) => {
+    return (version.match(versionRegex)?.slice(1, 4) ?? []).map(Number);
+};
 
 const resolveResult = (result: string) => {
     process.stdout.write(result, "utf-8");
@@ -13,22 +22,18 @@ if (!remoteVersion) {
     // new alias or re-creation of the old one
     version = localVersion;
 } else {
-    const [l_maj, l_min, l_patch] = (localVersion.match(versionRegex)?.slice(1, 4) ?? []).map(Number);
-    const [r_maj, r_min, r_patch] = (remoteVersion.match(versionRegex)?.slice(1, 4) ?? []).map(Number);
+    const [l_maj, l_min, l_patch] = getVersionNumbers(localVersion);
+    const [r_maj, r_min, r_patch] = getVersionNumbers(remoteVersion);
 
-    if (l_maj > r_maj || l_maj === r_maj && l_min > r_min || l_min === r_min && l_patch >= r_patch) {
+    if (
+        l_maj > r_maj ||
+        (l_maj === r_maj && l_min > r_min) ||
+        (l_maj === r_maj && l_min === r_min && l_patch >= r_patch)
+    ) {
         version = localVersion;
     } else {
         version = remoteVersion;
     }
-}
-
-const [candidate_maj, candidate_min, candidate_patch] = (version.match(versionRegex)?.slice(1, 4) ?? []).map(Number);
-const [repo_maj, repo_min, repo_patch] = (repoVersion.match(versionRegex)?.slice(1, 4) ?? []).map(Number);
-
-if (repo_maj === candidate_maj && repo_min === candidate_min && repo_patch === candidate_patch + 1) {
-    // take the latest repo version. "1.2.3-my-old-version.4"
-    version = repoVersion;
 }
 
 resolveResult(version);
--- scripts/switch-version.sh
+++ scripts/switch-version.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
+#
+# Select the latest current package version for an alias out of the local version and versions on the registry
 
 set -e
 
 export NODE_OPTIONS=--max_old_space_size=4096
 
-# Get tag name from the first argument
-tagName=$1;
-
-# Temporary files
-allTmp=$(mktemp);
-tmp=$(mktemp);
+# Name of branch or tag that was pushed
+ref_name=$1;
 
 previousVersion=$(cat package.json | jq -r .version);
 echo "Current package.json version is $previousVersion";
 
-npm show @anw/go-sdk-js versions --json > ${allTmp};
-#cat "$allTmp"
-jqparameter='[.[] | select(test("^\\d+\\.\\d+\\.\\d+-'$tagName'\\.\\d+$"))] | .[-1]'
+# Get all versions of given ref_name from the registry.
+all_registry_versions=$(mktemp);
+npm show @anw/go-sdk-js versions --json > ${all_registry_versions};
 
-# Get latest package version for given tag
-tag_v=$(npm dist-tag ls @anw/go-sdk-js | sed -n 's/^'"${tagName}"': //p');
-repo_v=$(jq -r "$jqparameter" "$allTmp");
+# Select the latest version of given ref_name.
+# Note: getting the latest version from all versions instead of the dist-tag prevents issues with
+# pushing existing versions when re-creating aliases.
+jq_parameter='[.[] | select(test("^\\d+\\.\\d+\\.\\d+-'$ref_name'\\.\\d+$"))] | .[-1] // empty'
+latest_registry_version=$(jq -r "$jq_parameter" "$all_registry_versions");
+echo "The latest version on the registry for the current alias is $latest_registry_version";
 
-echo "tag_v is $tag_v";
-echo "repo_v is $repo_v";
+# Select the latest version out of the local version and latest registry version.
+pkg_v=$(npx ts-node ./scripts/calculate-latest-version.ts "$previousVersion" "$latest_registry_version")
+echo "Current selected pre-release version is $pkg_v";
 
-pkg_v=$(npx ts-node ./scripts/calculate-latest-version.ts "$previousVersion" "$tag_v" "$repo_v")
-
-# Check current resolved version
-echo "Current pre-release version is $pkg_v";
-
-# Replace base version with resolved one, so the release script could calculate the next version
+# Replace local version with resolved one, so the release script could calculate the next version
+tmp=$(mktemp);
 jq --arg v "$pkg_v" '.version = $v' package.json > "$tmp" && mv "$tmp" package.json
 jq --arg v "$pkg_v" '.version = $v' package-lock.json > "$tmp" && mv "$tmp" package-lock.json
