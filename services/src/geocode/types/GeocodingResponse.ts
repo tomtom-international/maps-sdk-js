95f27b1a36066deadacb6d5a17ac1742d315c5cb
Suleyman Ali <suleyman.ali@tomtom.com>
2022-10-31 11:42:23 +0100

    refactor: change base type of GeocodingProps
    GitOrigin-RevId: dd30b35d3a3ee95bedf3ee0f1a3fbff172b18691

--- services/src/geocode/types/GeocodingResponse.ts
+++ services/src/geocode/types/GeocodingResponse.ts
@@ -1,4 +1,4 @@
-import { CommonPlaceProps, Places, PlaceType } from "@anw/go-sdk-js/core";
+import { SearchPlaceProps, Places, PlaceType } from "@anw/go-sdk-js/core";
 
 type GeocodingPlaceType = Exclude<PlaceType, "POI">;
 
@@ -6,21 +6,12 @@ type GeocodingPlaceType = Exclude<PlaceType, "POI">;
  * @group Geocoding
  * @category Types
  */
-export type GeocodingProps = CommonPlaceProps & {
+export type GeocodingProps = Omit<SearchPlaceProps, "info"> & {
     type: GeocodingPlaceType;
     /**
      * The confidence of the result`s textual match with the query.
      */
     matchConfidence: { score: number };
-    /**
-     * The score of the result.
-     * A larger score means there is a probability that a result meeting the query criteria is higher.
-     */
-    score?: number;
-    /**
-     * Unit: meters. This is the distance to an object if geobias was provided.
-     */
-    distance?: number;
 };
 
 /**
