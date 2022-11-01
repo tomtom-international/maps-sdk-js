5d99afaf1bcbf66f5288530addede4dc46b85df9
AyaSalama-TomTom <73919217+AyaSalama-TomTom@users.noreply.github.com>
2022-11-01 11:25:25 +0100

    chore: use native isArray and fix type casting
    Co-authored-by: Oleksii Popov <85178123+OleksiiPopov-TomTom@users.noreply.github.com>
    GitOrigin-RevId: e30e171432e4745802ca4418d260d0f406f2c97c

--- map/src/utils/localization.ts
+++ map/src/utils/localization.ts
@@ -1,14 +1,13 @@
 import { Map, SymbolLayerSpecification, ExpressionSpecification } from "maplibre-gl";
-import isArray from "lodash/isArray";
 
 export const isLayerLocalizable = (layer: SymbolLayerSpecification) => {
-    const textField = layer.layout?.["text-field"] as string | ExpressionSpecification;
+    const textField = (layer.layout?.["text-field"] ?? "") as string | ExpressionSpecification;
     // tries to detect layers which has "text-field" that can be localized
     // ex. "text-field": "{name}" or "text-field": ["get", "name"]
     const toBeLocalized = textField === "{name}" || (textField?.length == 2 && textField?.[1] == "name");
     // tries to detect layers which have "text-field" that was localized already
     // ex. "text-field": ["coalesce", ["get", "name_en-GB"], ["get", "name"]]
-    const isTextFieldAlreadyLocalized = isArray(textField?.[2]) && textField?.[2].includes("name");
+    const isTextFieldAlreadyLocalized = Array.isArray(textField?.[2]) && textField?.[2].includes("name");
     return toBeLocalized || isTextFieldAlreadyLocalized;
 };
 
