835499a0c3ee5d4e92fbe2cc82e466628c2d02f2
shanbhag <soorajrajkumar.shanbhag@tomtom.com>
2022-11-18 14:42:13 +0530

    (tests):#499 request builder & response parser performance tests for GeometrySearch
    GitOrigin-RevId: 162d93ef80740129708ca2a6d827f2799a515d13

---
+++ services/src/geometry-search/tests/performance/GeometrySearchPerformance.test.ts
@@ -0,0 +1,33 @@
+import { GeometrySearchParams, GeometrySearchResponseAPI } from "../../types";
+import { buildGeometrySearchRequest } from "../../RequestBuilder";
+import geometrySearchReqObjects from "../performance/RequestBuilderForPerf.data.json";
+import geometrySearchResponses from "../performance/ResponseParserForPerf.data.json";
+import { parseGeometrySearchResponse } from "../../ResponseParser";
+
+describe("Geometry Search request URL builder performance tests", () => {
+    const REFERENCE_KPI = 2; //1ms - needs to be agreed
+    test.each(geometrySearchReqObjects)(
+        "'%s'",
+        // @ts-ignore
+        (params: GeometrySearchParams) => {
+            const t0 = performance.now();
+            buildGeometrySearchRequest(params);
+            const t1 = performance.now();
+            expect(Number(`${t1 - t0}`)).toBeLessThanOrEqual(REFERENCE_KPI);
+        }
+    );
+});
+
+describe("Geometry Search response parser performance tests", () => {
+    const REFERENCE_KPI = 3; //3ms - needs to be agreed
+    test.each(geometrySearchResponses)(
+        "'%s'",
+        // @ts-ignore
+        (_name: title, apiResponse: GeometrySearchResponseAPI) => {
+            const t0 = performance.now();
+            parseGeometrySearchResponse(apiResponse);
+            const t1 = performance.now();
+            expect(Number(`${t1 - t0}`)).toBeLessThanOrEqual(REFERENCE_KPI);
+        }
+    );
+});
