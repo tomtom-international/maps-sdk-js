6c2c161ccf975b1c3ce8383ce0e2243a4ae49dcd
shanbhag <soorajrajkumar.shanbhag@tomtom.com>
2022-11-21 19:42:38 +0530

    minor refactoring
    GitOrigin-RevId: 82444f886f70f108b4fb5df0fca147fbefef3dc5

--- services/src/geometry-search/tests/performance/GeometrySearchPerformance.test.ts
+++ services/src/geometry-search/tests/performance/GeometrySearchPerformance.test.ts
@@ -5,45 +5,43 @@ import geometrySearchResponses from "../performance/ResponseParserForPerf.data.j
 import { parseGeometrySearchResponse } from "../../ResponseParser";
 
 describe("Geometry Search request URL builder performance tests", () => {
-    const maxExecTimeMS = 2; //2ms - needs to be agreed
+    const MAX_EXEC_TIME_MS = 5; //5ms - needs to be agreed
     test.each(geometrySearchReqObjects)(
         "'%s'",
         // @ts-ignore
-        (_name: name, params: GeometrySearchParams) => {
-            const timeTakenToExec = [];
+        (params: GeometrySearchParams) => {
+            const timeTakenToExec: number[] = [];
             //We run each test 10 times
             for (let i = 0; i < 10; i++) {
                 const t0 = performance.now();
                 buildGeometrySearchRequest(params);
                 const t1 = performance.now();
-                timeTakenToExec.push(`${t1 - t0}`);
+                timeTakenToExec.push(Number(`${t1 - t0}`));
             }
-            timeTakenToExec.sort();
             const p90Index = Math.round(0.9 * timeTakenToExec.length) - 1;
             //90th Percentile is considered
-            expect(Number(timeTakenToExec[p90Index])).toBeLessThanOrEqual(maxExecTimeMS);
+            expect(timeTakenToExec[p90Index]).toBeLessThanOrEqual(MAX_EXEC_TIME_MS);
         }
     );
 });
 
 describe("Geometry Search response parser performance tests", () => {
-    const maxExecTimeMS = 5; //5ms - needs to be agreed
+    const MAX_EXEC_TIME_MS = 5; //5ms - needs to be agreed
     test.each(geometrySearchResponses)(
         "'%s'",
         // @ts-ignore
         (_name: title, apiResponse: GeometrySearchResponseAPI) => {
-            const timeTakenToExec = [];
+            const timeTakenToExec: number[] = [];
             //We run each test 10 times
             for (let i = 0; i < 10; i++) {
                 const t0 = performance.now();
                 parseGeometrySearchResponse(apiResponse);
                 const t1 = performance.now();
-                timeTakenToExec.push(`${t1 - t0}`);
+                timeTakenToExec.push(Number(`${t1 - t0}`));
             }
-            timeTakenToExec.sort();
             const p90Index = Math.round(0.9 * timeTakenToExec.length) - 1;
             //90th Percentile is considered
-            expect(Number(timeTakenToExec[p90Index])).toBeLessThanOrEqual(maxExecTimeMS);
+            expect(timeTakenToExec[p90Index]).toBeLessThanOrEqual(MAX_EXEC_TIME_MS);
         }
     );
 });
