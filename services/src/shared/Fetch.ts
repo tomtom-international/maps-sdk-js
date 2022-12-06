aa1655ef9650e208c733fc20dace2e187c458da2
dafo <daniel.forniessoria@tomtom.com>
2022-12-06 16:42:52 +0100

    chore(services): upgrading axios to 1.2.0
    GitOrigin-RevId: c0a23836be33fe81b8286f5b803279252ef64e2a

--- services/src/shared/Fetch.ts
+++ services/src/shared/Fetch.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const AXIOS_CONFIG = { headers: { "accept-encoding": "application/json" } };
+
 /**
  * Fetches the given HTTP JSON resource with an HTTP GET request and returns a promise with the response as a JSON object.
  * If the response isn't successful it returns a rejected promise with the http error code.
@@ -7,7 +9,7 @@ import axios from "axios";
  * @param url The URL to fetch.
  */
 export const getJson = async <T>(url: URL): Promise<T> => {
-    const response = await axios.get(url.toString());
+    const response = await axios.get(url.toString(), AXIOS_CONFIG);
     return response.data;
 };
 
@@ -24,6 +26,6 @@ export type PostObject<D> = { url: URL; data?: D };
  * @param input The POST object with URL and optional payload.
  */
 export const postJson = async <T, D>(input: PostObject<D>): Promise<T> => {
-    const response = await axios.post(input.url.toString(), input.data);
+    const response = await axios.post(input.url.toString(), input.data, AXIOS_CONFIG);
     return response.data;
 };
