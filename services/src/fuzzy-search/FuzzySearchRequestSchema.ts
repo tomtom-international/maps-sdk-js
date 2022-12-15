167e86a8d4c2a4978f340fa2e0714bebd2e91183
Aya Salama <aya.salama@tomtom.com>
2022-12-15 14:39:20 +0100

    fix: geojson object validation schema
    GitOrigin-RevId: 2a926eb462f0471740c93a9cc1f24d029d146343

--- services/src/fuzzy-search/FuzzySearchRequestSchema.ts
+++ services/src/fuzzy-search/FuzzySearchRequestSchema.ts
@@ -2,8 +2,18 @@ import { z } from "zod";
 import { commonSearchParamsSchema } from "../search/CommonSearchParamsSchema";
 
 const simpleBBoxSchema = z.array(z.number()).length(4).or(z.array(z.number()).length(6));
-const geoJsonObjectWithBBoxSchema = z.object({
-    bbox: simpleBBoxSchema
+const geoJsonObjectSchema = z.object({
+    type: z.enum([
+        "Point",
+        "MultiPoint",
+        "LineString",
+        "MultiLineString",
+        "Polygon",
+        "MultiPolygon",
+        "GeometryCollection",
+        "Feature",
+        "FeatureCollection"
+    ])
 });
 const fuzzySearchRequestOptional = z
     .object({
@@ -11,7 +21,7 @@ const fuzzySearchRequestOptional = z
         offset: z.number(),
         countries: z.string().array(),
         radiusMeters: z.number(),
-        boundingBox: simpleBBoxSchema.or(geoJsonObjectWithBBoxSchema).or(z.array(geoJsonObjectWithBBoxSchema)),
+        boundingBox: simpleBBoxSchema.or(geoJsonObjectSchema).or(z.array(geoJsonObjectSchema)),
         minFuzzyLevel: z.number().min(1).max(4),
         mixFuzzyLevel: z.number().min(1).max(4)
     })
