a11350c15f9d8f0d80b8cf3af1cdd93e4dba47ca
Guilherme Scaldelai <guilherme.scaldelai@tomtom.com>
2023-01-20 19:13:22 +0100

    build: Adding scripts to run on CI env
    GitOrigin-RevId: 814ecfcec5c1fd25862b0d78356df02c16848af8

---
+++ scripts/prepare.js
@@ -0,0 +1,8 @@
+/* Prepare for npm installation */
+
+const isCI = process.env.CI !== undefined;
+
+if (!isCI) {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require("husky").install();
+}
