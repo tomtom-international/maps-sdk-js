8772c422faaaefe3339c9853f88bd06e7b2a17cf
Suleyman Ali <suleyman.ali@tomtom.com>
2023-05-11 11:26:49 +0200

    chore: sed compatibility between MacOS and Linux
    GitOrigin-RevId: 48a1ff829e90d9b1274a421ff1fb62652fe7fa89

--- scripts/documentation/update-sdk-version-in-docs.sh
+++ scripts/documentation/update-sdk-version-in-docs.sh
@@ -4,12 +4,21 @@ set -e
 
 VERSION=$(jq -r '.version' ./package.json)
 
-# Update typedoc.json frontmatter with new version label
+### Update typedoc.json frontmatter with new version label
 tmp=$(mktemp)
 jq "(.frontmatterGlobals.titleTags[] | select(.label | contains(\"VERSION\")) | .label) = \"VERSION $VERSION\"" ./typedoc.json > "$tmp" && mv "$tmp" ./typedoc.json
 echo "Updated typedoc.json frontmatter version label to VERSION $VERSION"
 
-# Update SDK version in static documentation (guides and overview)
-find ./documentation/dev-portal/javascript/maps/documentation/overview ./documentation/dev-portal/javascript/maps/documentation/guides \
- -type f -name "*.mdx" -exec sed -r -i '' "s/- label: \"VERSION [0-9]+\.[0-9]+\.[0-9]+\"/- label: \"VERSION $VERSION\"/" {} \;
+### Update SDK version in static documentation (guides and overview)
+SED_EXPRESSION="s/- label: \"VERSION [0-9]+\.[0-9]+\.[0-9]+\"/- label: \"VERSION $VERSION\"/"
+TARGET_FILES="./documentation/dev-portal/javascript/maps/documentation/overview ./documentation/dev-portal/javascript/maps/documentation/guides"
+
+# -i flag differs between sed for MacOS and Linux
+# Loop over all static docs (overview + guides) and replace the version label with the new version label
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find $TARGET_FILES -type f -name "*.mdx" -exec sed -r -i '' "$SED_EXPRESSION" {} +
+else
+  find $TARGET_FILES -type f -name "*.mdx" -exec sed -r -i "$SED_EXPRESSION" {} +
+fi
+
 echo "Updated guide version labels to VERSION $VERSION"
