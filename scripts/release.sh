734f2a0095a3d4b6ef2171faf168234a98bf880a
Suleyman Ali <suleyman.ali@tomtom.com>
2023-05-12 10:39:16 +0200

    chore: check if jq is installed before release
    GitOrigin-RevId: 20deb3a8a04d06f1528b938a5d03652689607947

---
+++ scripts/release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+[ "$(git rev-parse --abbrev-ref HEAD)" == "main" ] || { echo >&2 "Release is only permitted from the main branch. Aborting"; exit 1; }
+command -v jq >/dev/null 2>&1 || { echo >&2 "jq is required, but it's not installed. Please install jq before releasing (using Homebrew: 'brew install jq'). Aborting."; exit 1; }
+
+npx standard-version --commit-all
