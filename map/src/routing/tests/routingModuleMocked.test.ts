67044ef2a16d93d26347d802cd00eed85a4faf3a
Daniel Fornies <daniel.forniessoria@tomtom.com>
2023-12-27 20:40:57 +0100

    chore(routing): skipping test temporarily
    GitOrigin-RevId: fa54f64cc6e0137a69f3991d99f46ab53b75305a

--- map/src/routing/tests/routingModuleMocked.test.ts
+++ map/src/routing/tests/routingModuleMocked.test.ts
@@ -1,125 +1,125 @@
-import { Map } from "maplibre-gl";
-import {
-    EventsModule,
-    mapStyleLayerIDs,
-    ROUTE_FERRIES_SOURCE_ID,
-    ROUTE_INCIDENTS_SOURCE_ID,
-    ROUTE_INSTRUCTIONS_ARROWS_SOURCE_ID,
-    ROUTE_INSTRUCTIONS_SOURCE_ID,
-    ROUTE_TOLL_ROADS_SOURCE_ID,
-    ROUTE_TUNNELS_SOURCE_ID,
-    ROUTE_VEHICLE_RESTRICTED_SOURCE_ID,
-    ROUTES_SOURCE_ID,
-    WAYPOINTS_SOURCE_ID
-} from "../../shared";
-import { TomTomMap } from "../../TomTomMap";
-import { RoutingModule } from "../RoutingModule";
-import { routeDeselectedOutline } from "../layers/routeMainLineLayers";
-
-// NOTE: these tests are heavily mocked and are mostly used to keep coverage numbers high.
-// For real testing of such modules, refer to map-integration-tests.
-// Any forced coverage from tests here must be truly covered in map integration tests.
-// TODO: restore tests taking into account the embedded image assets!
-describe.skip("Routing module tests", () => {
-    // eslint-disable-next-line jest/expect-expect
-    test("Basic flows", async () => {
-        const waypointsSource = { id: WAYPOINTS_SOURCE_ID, setData: jest.fn() };
-        const routesSource = { id: ROUTES_SOURCE_ID, setData: jest.fn() };
-        const vehicleRestrictedSource = { id: ROUTE_VEHICLE_RESTRICTED_SOURCE_ID, setData: jest.fn() };
-        const incidentsSource = { id: ROUTE_INCIDENTS_SOURCE_ID, setData: jest.fn() };
-        const ferriesSource = { id: ROUTE_FERRIES_SOURCE_ID, setData: jest.fn() };
-        const tollRoadsSource = { id: ROUTE_TOLL_ROADS_SOURCE_ID, setData: jest.fn() };
-        const tunnelsSource = { id: ROUTE_TUNNELS_SOURCE_ID, setData: jest.fn() };
-        const instructionLinesSource = { id: ROUTE_INSTRUCTIONS_SOURCE_ID, setData: jest.fn() };
-        const instructionArrowsSource = { id: ROUTE_INSTRUCTIONS_ARROWS_SOURCE_ID, setData: jest.fn() };
-        const tomtomMapMock = {
-            mapLibreMap: {
-                getSource: jest
-                    .fn()
-                    .mockReturnValueOnce(waypointsSource)
-                    .mockReturnValueOnce(waypointsSource)
-                    .mockReturnValueOnce(waypointsSource)
-                    .mockReturnValueOnce(waypointsSource)
-                    .mockReturnValueOnce(routesSource)
-                    .mockReturnValueOnce(routesSource)
-                    .mockReturnValueOnce(routesSource)
-                    .mockReturnValueOnce(routesSource)
-                    .mockReturnValueOnce(vehicleRestrictedSource)
-                    .mockReturnValueOnce(vehicleRestrictedSource)
-                    .mockReturnValueOnce(vehicleRestrictedSource)
-                    .mockReturnValueOnce(vehicleRestrictedSource)
-                    .mockReturnValueOnce(incidentsSource)
-                    .mockReturnValueOnce(incidentsSource)
-                    .mockReturnValueOnce(incidentsSource)
-                    .mockReturnValueOnce(incidentsSource)
-                    .mockReturnValueOnce(ferriesSource)
-                    .mockReturnValueOnce(ferriesSource)
-                    .mockReturnValueOnce(ferriesSource)
-                    .mockReturnValueOnce(ferriesSource)
-                    .mockReturnValueOnce(tollRoadsSource)
-                    .mockReturnValueOnce(tollRoadsSource)
-                    .mockReturnValueOnce(tollRoadsSource)
-                    .mockReturnValueOnce(tollRoadsSource)
-                    .mockReturnValueOnce(tunnelsSource)
-                    .mockReturnValueOnce(tunnelsSource)
-                    .mockReturnValueOnce(tunnelsSource)
-                    .mockReturnValueOnce(tunnelsSource)
-                    .mockReturnValueOnce(instructionLinesSource)
-                    .mockReturnValueOnce(instructionLinesSource)
-                    .mockReturnValueOnce(instructionLinesSource)
-                    .mockReturnValueOnce(instructionLinesSource)
-                    .mockReturnValueOnce(instructionArrowsSource)
-                    .mockReturnValueOnce(instructionArrowsSource)
-                    .mockReturnValueOnce(instructionArrowsSource)
-                    .mockReturnValueOnce(instructionArrowsSource),
-
-                getLayer: jest.fn().mockReturnValue({}),
-                addLayer: jest.fn(),
-                removeLayer: jest.fn(),
-                hasImage: jest.fn().mockReturnValue(false),
-                loadImage: jest.fn(),
-                setLayoutProperty: jest.fn(),
-                setFilter: jest.fn(),
-                setPaintProperty: jest.fn()
-            } as unknown as Map,
-            _eventsProxy: {
-                add: jest.fn(),
-                ensureAdded: jest.fn()
-            },
-            addStyleChangeHandler: jest.fn(),
-            once: jest.fn().mockReturnValue(Promise.resolve()),
-            mapReady: jest.fn().mockReturnValue(false).mockReturnValue(true)
-        } as unknown as TomTomMap;
-
-        const routing = await RoutingModule.init(tomtomMapMock);
-        routing.showRoutes({ type: "FeatureCollection", features: [] });
-        routing.selectRoute(0);
-        routing.clearRoutes();
-        routing.showWaypoints([]);
-        routing.showWaypoints({ type: "FeatureCollection", features: [] });
-        routing.clearWaypoints();
-        expect(routing.getLayerToRenderLinesUnder()).toStrictEqual(mapStyleLayerIDs.lowestLabel);
-        routing.applyConfig({
-            routeLayers: {
-                mainLine: {
-                    layers: [
-                        {
-                            id: "a different id",
-                            layerSpec: routeDeselectedOutline,
-                            beforeID: mapStyleLayerIDs.lowestLabel
-                        }
-                    ]
-                }
-            }
-        });
-
-        expect(routing.events.routeLines).toBeInstanceOf(EventsModule);
-        expect(routing.events.waypoints).toBeInstanceOf(EventsModule);
-        expect(routing.events.vehicleRestricted).toBeInstanceOf(EventsModule);
-        expect(routing.events.incidents).toBeInstanceOf(EventsModule);
-        expect(routing.events.ferries).toBeInstanceOf(EventsModule);
-        expect(routing.events.tollRoads).toBeInstanceOf(EventsModule);
-        expect(routing.events.tunnels).toBeInstanceOf(EventsModule);
-        expect(routing.events.instructionLines).toBeInstanceOf(EventsModule);
-    });
-});
+// import { Map } from "maplibre-gl";
+// import {
+//     EventsModule,
+//     mapStyleLayerIDs,
+//     ROUTE_FERRIES_SOURCE_ID,
+//     ROUTE_INCIDENTS_SOURCE_ID,
+//     ROUTE_INSTRUCTIONS_ARROWS_SOURCE_ID,
+//     ROUTE_INSTRUCTIONS_SOURCE_ID,
+//     ROUTE_TOLL_ROADS_SOURCE_ID,
+//     ROUTE_TUNNELS_SOURCE_ID,
+//     ROUTE_VEHICLE_RESTRICTED_SOURCE_ID,
+//     ROUTES_SOURCE_ID,
+//     WAYPOINTS_SOURCE_ID
+// } from "../../shared";
+// import { TomTomMap } from "../../TomTomMap";
+// import { RoutingModule } from "../RoutingModule";
+// import { routeDeselectedOutline } from "../layers/routeMainLineLayers";
+//
+// // NOTE: these tests are heavily mocked and are mostly used to keep coverage numbers high.
+// // For real testing of such modules, refer to map-integration-tests.
+// // Any forced coverage from tests here must be truly covered in map integration tests.
+// // TODO: restore tests taking into account the embedded image assets!
+// describe("Routing module tests", () => {
+//     // eslint-disable-next-line jest/expect-expect
+//     test("Basic flows", async () => {
+//         const waypointsSource = { id: WAYPOINTS_SOURCE_ID, setData: jest.fn() };
+//         const routesSource = { id: ROUTES_SOURCE_ID, setData: jest.fn() };
+//         const vehicleRestrictedSource = { id: ROUTE_VEHICLE_RESTRICTED_SOURCE_ID, setData: jest.fn() };
+//         const incidentsSource = { id: ROUTE_INCIDENTS_SOURCE_ID, setData: jest.fn() };
+//         const ferriesSource = { id: ROUTE_FERRIES_SOURCE_ID, setData: jest.fn() };
+//         const tollRoadsSource = { id: ROUTE_TOLL_ROADS_SOURCE_ID, setData: jest.fn() };
+//         const tunnelsSource = { id: ROUTE_TUNNELS_SOURCE_ID, setData: jest.fn() };
+//         const instructionLinesSource = { id: ROUTE_INSTRUCTIONS_SOURCE_ID, setData: jest.fn() };
+//         const instructionArrowsSource = { id: ROUTE_INSTRUCTIONS_ARROWS_SOURCE_ID, setData: jest.fn() };
+//         const tomtomMapMock = {
+//             mapLibreMap: {
+//                 getSource: jest
+//                     .fn()
+//                     .mockReturnValueOnce(waypointsSource)
+//                     .mockReturnValueOnce(waypointsSource)
+//                     .mockReturnValueOnce(waypointsSource)
+//                     .mockReturnValueOnce(waypointsSource)
+//                     .mockReturnValueOnce(routesSource)
+//                     .mockReturnValueOnce(routesSource)
+//                     .mockReturnValueOnce(routesSource)
+//                     .mockReturnValueOnce(routesSource)
+//                     .mockReturnValueOnce(vehicleRestrictedSource)
+//                     .mockReturnValueOnce(vehicleRestrictedSource)
+//                     .mockReturnValueOnce(vehicleRestrictedSource)
+//                     .mockReturnValueOnce(vehicleRestrictedSource)
+//                     .mockReturnValueOnce(incidentsSource)
+//                     .mockReturnValueOnce(incidentsSource)
+//                     .mockReturnValueOnce(incidentsSource)
+//                     .mockReturnValueOnce(incidentsSource)
+//                     .mockReturnValueOnce(ferriesSource)
+//                     .mockReturnValueOnce(ferriesSource)
+//                     .mockReturnValueOnce(ferriesSource)
+//                     .mockReturnValueOnce(ferriesSource)
+//                     .mockReturnValueOnce(tollRoadsSource)
+//                     .mockReturnValueOnce(tollRoadsSource)
+//                     .mockReturnValueOnce(tollRoadsSource)
+//                     .mockReturnValueOnce(tollRoadsSource)
+//                     .mockReturnValueOnce(tunnelsSource)
+//                     .mockReturnValueOnce(tunnelsSource)
+//                     .mockReturnValueOnce(tunnelsSource)
+//                     .mockReturnValueOnce(tunnelsSource)
+//                     .mockReturnValueOnce(instructionLinesSource)
+//                     .mockReturnValueOnce(instructionLinesSource)
+//                     .mockReturnValueOnce(instructionLinesSource)
+//                     .mockReturnValueOnce(instructionLinesSource)
+//                     .mockReturnValueOnce(instructionArrowsSource)
+//                     .mockReturnValueOnce(instructionArrowsSource)
+//                     .mockReturnValueOnce(instructionArrowsSource)
+//                     .mockReturnValueOnce(instructionArrowsSource),
+//
+//                 getLayer: jest.fn().mockReturnValue({}),
+//                 addLayer: jest.fn(),
+//                 removeLayer: jest.fn(),
+//                 hasImage: jest.fn().mockReturnValue(false),
+//                 loadImage: jest.fn(),
+//                 setLayoutProperty: jest.fn(),
+//                 setFilter: jest.fn(),
+//                 setPaintProperty: jest.fn()
+//             } as unknown as Map,
+//             _eventsProxy: {
+//                 add: jest.fn(),
+//                 ensureAdded: jest.fn()
+//             },
+//             addStyleChangeHandler: jest.fn(),
+//             once: jest.fn().mockReturnValue(Promise.resolve()),
+//             mapReady: jest.fn().mockReturnValue(false).mockReturnValue(true)
+//         } as unknown as TomTomMap;
+//
+//         const routing = await RoutingModule.init(tomtomMapMock);
+//         routing.showRoutes({ type: "FeatureCollection", features: [] });
+//         routing.selectRoute(0);
+//         routing.clearRoutes();
+//         routing.showWaypoints([]);
+//         routing.showWaypoints({ type: "FeatureCollection", features: [] });
+//         routing.clearWaypoints();
+//         expect(routing.getLayerToRenderLinesUnder()).toStrictEqual(mapStyleLayerIDs.lowestLabel);
+//         routing.applyConfig({
+//             routeLayers: {
+//                 mainLine: {
+//                     layers: [
+//                         {
+//                             id: "a different id",
+//                             layerSpec: routeDeselectedOutline,
+//                             beforeID: mapStyleLayerIDs.lowestLabel
+//                         }
+//                     ]
+//                 }
+//             }
+//         });
+//
+//         expect(routing.events.routeLines).toBeInstanceOf(EventsModule);
+//         expect(routing.events.waypoints).toBeInstanceOf(EventsModule);
+//         expect(routing.events.vehicleRestricted).toBeInstanceOf(EventsModule);
+//         expect(routing.events.incidents).toBeInstanceOf(EventsModule);
+//         expect(routing.events.ferries).toBeInstanceOf(EventsModule);
+//         expect(routing.events.tollRoads).toBeInstanceOf(EventsModule);
+//         expect(routing.events.tunnels).toBeInstanceOf(EventsModule);
+//         expect(routing.events.instructionLines).toBeInstanceOf(EventsModule);
+//     });
+// });
