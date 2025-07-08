fac7808d61bf07221eeb8f97d197e9a618292d29
Daniel Fornies <daniel.forniessoria@tomtom.com>
2025-07-08 13:08:33 +0200

    fix: revert script accidental change
    GitOrigin-RevId: 9c07c2b40d5f4ba4a9e13b43ef30f534a00e8744

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-[ "$(git rev-parse --abbrev-ref HEAD)" === "main" ] || { echo >&2 "Release is only permitted from the main branch. Aborting"; exit 1; }
+[ "$(git rev-parse --abbrev-ref HEAD)" == "main" ] || { echo >&2 "Release is only permitted from the main branch. Aborting"; exit 1; }
 command -v jq >/dev/null 2>&1 || { echo >&2 "jq is required, but it's not installed. Please install jq before releasing (using Homebrew: 'brew install jq'). Aborting."; exit 1; }
 
 npx standard-version --commit-all
