7004eddfa3ba87081cc796402aad768d38c74f61
Daniel Fornies <daniel.forniessoria@tomtom.com>
2025-08-25 15:13:53 +0200

    chore: lower playwright workers for stability
    GitOrigin-RevId: b71d6a305271c2bf4104d8e379e01208702d7daf

--- map-integration-tests/playwright.config.ts
+++ map-integration-tests/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     fullyParallel: true,
-    workers: 3,
+    workers: 2,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: process.env.CI ? 'list' : 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
