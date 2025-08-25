690ddf9a7bc134cad0b920f5b983ed0a55dfb458
Daniel Fornies <daniel.forniessoria@tomtom.com>
2025-08-25 11:41:11 +0200

    chore: use more powerful runner for CI builds
    GitOrigin-RevId: 60d1e0376e8d83afa55feea4a35352d404ee8e63

--- map-integration-tests/playwright.config.ts
+++ map-integration-tests/playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     fullyParallel: true,
-    workers: 2,
+    workers: 3,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: process.env.CI ? 'list' : 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
