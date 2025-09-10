60406dae8077239bef646e5e6aef3bfbc5af6062
Karthik SM <karthik.sekar@tomtom.com>
2025-09-10 17:20:25 +0200

    test: make changes to generate single file for dist
    GitOrigin-RevId: 881145d28ce8ef0316594366b9ebef2e9e8313fb

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,33 +1,39 @@
-import path from 'node:path';
-import { visualizer } from 'rollup-plugin-visualizer';
-import { defineConfig, loadEnv } from 'vite';
+import path from "node:path";
+import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig, loadEnv } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // NOTE: This config is meant to be reused by each example. Thus, any configured paths are likely relatively to each example folder.
 export default defineConfig(({ mode }) => {
-    return {
-        root: '.',
-        base: './',
-        build: { emptyOutDir: true },
-        plugins: [
-            ...(process.env.CI
-                ? []
-                : [
-                      visualizer({
-                          filename: 'bundle-stats.html',
-                          open: false,
-                          gzipSize: true,
-                      }),
-                  ]),
-        ],
-        server: { port: 9022 },
-        resolve: {
-            alias: {
-                // We ensure to locally alias imports from @cet/maps-sdk-js/core from the SDK code itself to the locally built core package.
-                '@cet/maps-sdk-js/core': path.resolve('../../core/dist/core.es.js'),
-            },
-        },
-        define: {
-            'process.env': JSON.stringify(loadEnv(mode, path.resolve('..'), '')),
-        },
-    };
+  return {
+    root: ".",
+    base: "./",
+    build: {
+      target: "esnext",
+      minify: "esbuild",
+      assetsInlineLimit: Number.POSITIVE_INFINITY, // Inline all assets },
+    },
+    plugins: [
+      ...(process.env.CI
+        ? []
+        : [
+            visualizer({
+              filename: "bundle-stats.html",
+              open: false,
+              gzipSize: true,
+            }),
+          ]),
+      viteSingleFile({removeViteModuleLoader: true}),
+    ],
+    server: { port: 9022 },
+    resolve: {
+      alias: {
+        // We ensure to locally alias imports from @cet/maps-sdk-js/core from the SDK code itself to the locally built core package.
+        "@cet/maps-sdk-js/core": path.resolve("../../core/dist/core.es.js"),
+      },
+    },
+    define: {
+      "process.env": JSON.stringify(loadEnv(mode, path.resolve(".."), "")),
+    },
+  };
 });
