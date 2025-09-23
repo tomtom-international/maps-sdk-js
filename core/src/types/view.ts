8e588cc491d3af510c237411cc92d19d49f88828
Daniel Fornies <daniel.forniessoria@tomtom.com>
2025-09-23 15:15:40 +0200

    fix: add missing typedoc
    GitOrigin-RevId: 243716f96f0dd633326bbbfbd93a96a598b88255

--- core/src/types/view.ts
+++ core/src/types/view.ts
@@ -1,9 +1,12 @@
 /**
+ * List of available views for geopolitical context.
  * @group Shared
  * @category Types
  */
 export const views = ['Unified', 'AR', 'IN', 'PK', 'IL', 'MA', 'RU', 'TR', 'CN'] as const;
+
 /**
+ * Geopolitical View type. The context used to resolve the handling of disputed territories.
  * @group Shared
  * @category Types
  */
