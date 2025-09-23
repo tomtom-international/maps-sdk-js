bb2f34f2ce3ee4c3d9f120a885a0e3728579c614
Karthik SM <karthik.sekar@tomtom.com>
2025-09-23 15:35:31 +0200

    feat: use version of the root package.json
    GitOrigin-RevId: cb3b277c979eeba320f60ffdce78d4af94788071

--- scripts/copy-maps-sdk-js-examples-dist.sh
+++ scripts/copy-maps-sdk-js-examples-dist.sh
@@ -27,3 +27,13 @@ for module_path in "$EXAMPLES_DIR"/*; do
 done
 
 echo "✅ All done."
+
+# Get root version
+ROOT_VERSION=$(node -p "require('./package.json').version")
+
+# Set path to subfolder package
+SUBFOLDER="./examples-dist"
+
+# Update version in subfolder package.json
+jq --arg version "$ROOT_VERSION" '.version = $version' "$SUBFOLDER/package.json" > "$SUBFOLDER/package.tmp.json" && \
+mv "$SUBFOLDER/package.tmp.json" "$SUBFOLDER/package.json"
