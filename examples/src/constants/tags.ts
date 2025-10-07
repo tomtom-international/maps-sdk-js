e73f3d37ad0a91119fed2953700c197c51837f4b
Karthik SM <karthik.sekar@tomtom.com>
2025-10-07 14:18:57 +0200

    feat: restructure tags filter
    GitOrigin-RevId: c96387f8d145c6881c5aef720d6d5f0e916859d1

--- examples/src/constants/tags.ts
+++ examples/src/constants/tags.ts
@@ -1,72 +1,111 @@
-// TODO: make this driven by SDK itself (do not redefine here)
-
-const TAG_IDS = [
-    'map',
-    'fuzzy-search',
-    'playground',
-    'web',
-    'map-pois',
-    'poi-search',
-    'base-map',
-    'geometry-search',
-    'geocoding',
-    'map-style',
-    'route-planning',
-    'place-geometry',
-    'localization',
-    'nodejs',
-    'rev-geocoding',
-    'places',
-    'maplibre-customization',
-    'reachable-range',
-    'traffic',
-];
-const TAG_GROUP_IDS = ['product', 'feature', 'use-case', 'technology'];
-
-export type TagId = (typeof TAG_IDS)[number];
-export type TagGroupId = (typeof TAG_GROUP_IDS)[number];
-
-export const TAG_LABEL: Record<TagId, string> = {
-    map: 'Map',
-    'fuzzy-search': 'Fuzzy search',
-    playground: 'Playground',
-    web: 'Web',
-    'map-pois': 'Map POIs',
-    'poi-search': 'POI search',
-    'base-map': 'Base map',
-    'geometry-search': 'Geometry search',
-    geocoding: 'Geocoding',
-    'map-style': 'Map style',
-    'route-planning': 'Route planning',
-    'place-geometry': 'Place geometry',
-    localization: 'Localization',
-    nodejs: 'Node.js',
-    'rev-geocoding': 'Reverse geocoding',
-    places: 'Places',
-    'maplibre-customization': 'MapLibre customization',
-    'reachable-range': 'Reachable range',
-    traffic: 'Traffic',
+export type Tag = {
+  id: string;
+  label: string;
 };
 
-export const TAG_GROUP_LABEL: Record<TagGroupId, string> = {
-    product: 'Product group',
-    feature: 'Features/Modules',
-    'use-case': 'Use case/topic',
-    technology: 'Technology',
-};
+export type TagGroup = {
+  tags: Tag[];
+} & Tag;
 
-export const TAGS_BY_GROUP: Record<TagGroupId, TagId[]> = {
-    product: ['map', 'places', 'traffic'],
-    feature: ['fuzzy-search', 'map-pois', 'poi-search', 'geometry-search', 'place-geometry', 'reachable-range'],
-    'use-case': ['route-planning', 'localization'],
-    technology: [
-        'playground',
-        'web',
-        'base-map',
-        'geocoding',
-        'map-style',
-        'nodejs',
-        'rev-geocoding',
-        'maplibre-customization',
+export const TAG_GROUP: TagGroup[] = [
+  {
+    id: "product",
+    label: "Product group",
+    tags: [
+      {
+        id: "map",
+        label: "Map",
+      },
+      {
+        id: "places",
+        label: "Places",
+      },
+      {
+        id: "traffic",
+        label: "Traffic",
+      },
     ],
-};
+  },
+  {
+    id: "feature",
+    label: "Features/Modules",
+    tags: [
+      {
+        id: "fuzzy-search",
+        label: "Fuzzy search",
+      },
+      {
+        id: "map-pois",
+        label: "Map POIs",
+      },
+      {
+        id: "poi-search",
+        label: "POI search",
+      },
+      {
+        id: "geometry-search",
+        label: "Geometry search",
+      },
+      {
+        id: "place-geometry",
+        label: "Place geometry",
+      },
+      {
+        id: "reachable-range",
+        label: "Reachable range",
+      },
+    ],
+  },
+  {
+    id: "use-case",
+    label: "Use case/topic",
+    tags: [
+      {
+        id: "route-planning",
+        label: "Route planning",
+      },
+      {
+        id: "localization",
+        label: "Localization",
+      },
+    ],
+  },
+  {
+    id: "technology",
+    label: "Technology",
+    tags: [
+      {
+        id: "playground",
+        label: "Playground",
+      },
+      {
+        id: "web",
+        label: "Web",
+      },
+      {
+        id: "base-map",
+        label: "Base map",
+      },
+      {
+        id: "geocoding",
+        label: "Geocoding",
+      },
+      {
+        id: "map-style",
+        label: "Map style",
+      },
+      {
+        id: "nodejs",
+        label: "Node.js",
+      },
+      {
+        id: "rev-geocoding",
+        label: "Reverse geocoding",
+      },
+      {
+        id: "maplibre-customization",
+        label: "MapLibre customization",
+      },
+    ],
+  },
+];
