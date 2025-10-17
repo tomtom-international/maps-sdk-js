a2a234b2d3e8e232dd272f2eaab6624b0af294aa
Daniel Fornies <daniel.forniessoria@tomtom.com>
2025-10-17 15:34:33 +0200

    chore: remove redundant alias for core module vitest config
    GitOrigin-RevId: a283c385577bd8fb76eaa0774fdd5d8b28e77301

--- core/vitest.config.ts
+++ core/vitest.config.ts
@@ -4,9 +4,4 @@ import commonVitestConfig from '../shared-configs/vitest.config';
 /// <reference types="vitest" />
 export default defineConfig({
     ...commonVitestConfig,
-    resolve: {
-        alias: {
-            '@cet/maps-sdk-js/core': 'core',
-        },
-    },
 });
