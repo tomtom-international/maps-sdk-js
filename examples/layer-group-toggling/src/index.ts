363f22be1babcd320b4f371967725c809a5668d2
Daniel Fornies <daniel.forniessoria@tomtom.com>
2025-11-02 21:15:53 +0100

    fix: renamed variable to avoid confusion
    GitOrigin-RevId: 755160f79f709a24805b8bf2d55ec2af526878c6

--- examples/layer-group-toggling/src/index.ts
+++ examples/layer-group-toggling/src/index.ts
@@ -13,16 +13,9 @@ import './style.css';
 // (Set your own API key when working in your own environment)
 TomTomConfig.instance.put({ apiKey: process.env.API_KEY_EXAMPLES, language: 'en-GB' });
 
-const initBaseMapModule = async (layerGroups: BaseMapLayerGroupName[], selector: string) => {
-    const module = await BaseMapModule.get(map, { layerGroupsFilter: { mode: 'include', names: layerGroups } });
-    document
-        .querySelector(selector)
-        ?.addEventListener('click', (ev) => module.setVisible((ev.target as HTMLInputElement).checked));
-};
-
 const map = new TomTomMap({ container: 'maps-sdk-js-examples-map-container', center: [-74.06332, 40.72732], zoom: 12 });
 
-const modules: BaseMapLayerGroupName[] = [
+const layerGroups: BaseMapLayerGroupName[] = [
     'water',
     'land',
     'borders',
@@ -39,17 +32,22 @@ const modules: BaseMapLayerGroupName[] = [
     'stateLabels',
     'countryLabels',
 ];
-for (const module of modules) {
-    await initBaseMapModule([module], `#maps-sdk-js-examples-toggle-${module}`);
+
+for (const layerGroup of layerGroups) {
+    const module = await BaseMapModule.get(map, { layerGroupsFilter: { mode: 'include', names: layerGroups } });
+    document
+        .querySelector(`#maps-sdk-js-examples-toggle-${layerGroup}`)
+        ?.addEventListener('click', (ev) => module.setVisible((ev.target as HTMLInputElement).checked));
 }
 const pois = await POIsModule.get(map);
 document
     .querySelector('#maps-sdk-js-examples-togglePOIs')
     ?.addEventListener('click', (ev) => pois.setVisible((ev.target as HTMLInputElement).checked));
 
-// style selector:
 const stylesSelector = document.querySelector('#maps-sdk-js-examples-mapStyles') as HTMLSelectElement;
-standardStyleIDs.forEach((id) => stylesSelector.add(new Option(id)));
+for (const id of standardStyleIDs) {
+    stylesSelector.add(new Option(id));
+}
 stylesSelector.addEventListener('change', (event) =>
     map.setStyle((event.target as HTMLOptionElement).value as StandardStyleID),
 );
