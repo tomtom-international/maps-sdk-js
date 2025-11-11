f1aab9de87a4a6fecb774aeab3c3dc26a921f288
Daniel Fornies <daniel.forniessoria@tomtom.com>
2025-11-11 15:02:11 +0100

    chore: type casting
    GitOrigin-RevId: 698ae9f523d2e247130b109f79969cbe5572f879

--- examples/reverse-geocode/src/index.ts
+++ examples/reverse-geocode/src/index.ts
@@ -2,6 +2,7 @@ import { TomTomConfig } from '@tomtom-org/maps-sdk/core';
 import { PlacesModule, TomTomMap } from '@tomtom-org/maps-sdk/map';
 import { reverseGeocode } from '@tomtom-org/maps-sdk/services';
 import './style.css';
+import type { LngLatLike } from 'maplibre-gl';
 
 // (Set your own API key when working in your own environment)
 TomTomConfig.instance.put({ apiKey: process.env.API_KEY_EXAMPLES });
@@ -10,7 +11,7 @@ const location = await reverseGeocode({ position: [5.112969, 52.090521] });
 
 const map = new TomTomMap({
     container: 'maps-sdk-js-examples-map-container',
-    center: location.geometry.coordinates,
+    center: location.geometry.coordinates as LngLatLike,
     zoom: 17,
 });
 
