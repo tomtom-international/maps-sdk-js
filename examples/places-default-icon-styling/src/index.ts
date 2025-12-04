4841b55832490511845145aaffcf40220644f3f5
Daniel Fornies <daniel.forniessoria@tomtom.com>
2025-12-04 11:48:54 +0100

    chore: add missing iife so it runs on sandpack
    GitOrigin-RevId: 77d43a64cc9d94df8b27b2d6baf6ef2f17a32d02

--- examples/places-default-icon-styling/src/index.ts
+++ examples/places-default-icon-styling/src/index.ts
@@ -7,29 +7,31 @@ import { API_KEY } from './config';
 // (Set your own API key when working in your own environment)
 TomTomConfig.instance.put({ apiKey: API_KEY, language: 'en-US' });
 
-const [firstGroup, secondGroup, thirdGroup] = await Promise.all([
-    geocode({ query: '1051', countries: ['NL'] }), // searching for locations by postcode
-    geocode({ query: '1052', countries: ['NL'] }),
-    geocode({ query: '1053', countries: ['NL'] }),
-]);
+(async () => {
+    const [firstGroup, secondGroup, thirdGroup] = await Promise.all([
+        geocode({ query: '1051', countries: ['NL'] }), // searching for locations by postcode
+        geocode({ query: '1052', countries: ['NL'] }),
+        geocode({ query: '1053', countries: ['NL'] }),
+    ]);
 
-const map = new TomTomMap({
-    container: 'sdk-map',
-    bounds: bboxFromGeoJSON([firstGroup, secondGroup]),
-    fitBoundsOptions: { padding: 50 },
-});
+    const map = new TomTomMap({
+        container: 'sdk-map',
+        bounds: bboxFromGeoJSON([firstGroup, secondGroup]),
+        fitBoundsOptions: { padding: 50 },
+    });
 
-const firstPlacesModule = await PlacesModule.get(map, {
-    icon: { default: { style: { fillColor: '#FFBF00', outlineColor: '#113300', outlineOpacity: 0.25 } } },
-});
-await firstPlacesModule.show(firstGroup);
+    const firstPlacesModule = await PlacesModule.get(map, {
+        icon: { default: { style: { fillColor: '#FFBF00', outlineColor: '#113300', outlineOpacity: 0.25 } } },
+    });
+    await firstPlacesModule.show(firstGroup);
 
-const secondPlacesModule = await PlacesModule.get(map, {
-    icon: { default: { style: { fillColor: 'lightblue', outlineColor: 'grey', outlineOpacity: 0.5 } } },
-});
-await secondPlacesModule.show(secondGroup);
+    const secondPlacesModule = await PlacesModule.get(map, {
+        icon: { default: { style: { fillColor: 'lightblue', outlineColor: 'grey', outlineOpacity: 0.5 } } },
+    });
+    await secondPlacesModule.show(secondGroup);
 
-const thirdPlacesModule = await PlacesModule.get(map, {
-    icon: { default: { style: { fillColor: '#FFBBCC', outlineColor: 'red', outlineOpacity: 0.25 } } },
-});
-await thirdPlacesModule.show(thirdGroup);
+    const thirdPlacesModule = await PlacesModule.get(map, {
+        icon: { default: { style: { fillColor: '#FFBBCC', outlineColor: 'red', outlineOpacity: 0.25 } } },
+    });
+    await thirdPlacesModule.show(thirdGroup);
+})();
