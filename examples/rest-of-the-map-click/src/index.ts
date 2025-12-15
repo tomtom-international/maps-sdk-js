50dfded1b7ea97807e8dde1adf1d0cbdd18e7dee
Daniel Fornies <daniel.forniessoria@tomtom.com>
2025-12-15 17:49:29 +0100

    chore: new example to showcase how to click away from a known feature
    GitOrigin-RevId: 39756242cc15441d2f5550d11217e53af1f8e02c

---
+++ examples/rest-of-the-map-click/src/index.ts
@@ -0,0 +1,36 @@
+import type { Place } from '@tomtom-org/maps-sdk/core';
+import { TomTomConfig } from '@tomtom-org/maps-sdk/core';
+import { BaseMapModule, PlacesModule, TomTomMap } from '@tomtom-org/maps-sdk/map';
+import { searchOne } from '@tomtom-org/maps-sdk/services';
+import './style.css';
+import { API_KEY } from './config';
+
+// (Set your own API key when working in your own environment)
+TomTomConfig.instance.put({ apiKey: API_KEY, language: 'en-US' });
+
+const infoPanel = document.getElementById('info-panel') as HTMLElement;
+
+(async () => {
+    const place = (await searchOne('British Museum, London')) as Place;
+
+    const map = new TomTomMap({
+        container: 'sdk-map',
+        center: place.geometry.coordinates as [number, number],
+        zoom: 13,
+    });
+
+    const placesModule = await PlacesModule.get(map);
+    await placesModule.show(place);
+
+    const baseMap = await BaseMapModule.get(map, {
+        events: { cursorOnHover: 'default' }, // Keep default cursor on background
+    });
+
+    placesModule.events.on('click', (place) => {
+        infoPanel.textContent = `Clicked on: ${place.properties.title}`;
+    });
+
+    baseMap.events.on('click', () => {
+        infoPanel.textContent = 'Clicked on the rest of the map';
+    });
+})();
