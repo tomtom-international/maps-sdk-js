093295621205cd1298e628bba651159388dce5a9
Daniel Fornies <daniel.forniessoria@tomtom.com>
2025-12-16 12:09:20 +0100

    chore: add sandpack support in new example
    GitOrigin-RevId: 7379c1c21d4dbdf86c1cee9ec064187c4839f8d0

---
+++ examples/rest-of-the-map-click/sandpack/index.tsx
@@ -0,0 +1,18 @@
+import { SandpackWrapper } from '../../src/sandpack/SandpackWrapper';
+import { getSandpackDependencies, getSandpackFiles } from '../../src/utils/sandpackUtils';
+
+export default function RestOfTheMapClick() {
+    const { layout, files } = getSandpackFiles();
+
+    return (
+        <SandpackWrapper
+            customSetup={{
+                dependencies: getSandpackDependencies(),
+            }}
+            options={{
+                layout,
+            }}
+            files={files}
+        />
+    );
+}
---
+++ examples/rest-of-the-map-click/sandpackPreview/index.tsx
@@ -0,0 +1,4 @@
+import { renderSandpackPreview } from '../../src/sandpackPreview';
+import RestOfTheMapClick from '../sandpack';
+
+renderSandpackPreview(<RestOfTheMapClick />);
