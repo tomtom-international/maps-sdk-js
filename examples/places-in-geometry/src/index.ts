fc4bc0ea28609bd9868e740cca08dba02f844b63
Daniel Fornies <daniel.forniessoria@tomtom.com>
2025-12-17 12:00:41 +0100

    chore: fix types and improve places in geometry example
    GitOrigin-RevId: a6d09e114f135f0748c40ee30c4346d607c6cdd2

--- examples/places-in-geometry/src/index.ts
+++ examples/places-in-geometry/src/index.ts
@@ -1,18 +1,17 @@
-import { TomTomConfig } from '@tomtom-org/maps-sdk/core';
+import { type BBox, PolygonFeature, TomTomConfig } from '@tomtom-org/maps-sdk/core';
 import { GeometriesModule, PlacesModule, TomTomMap } from '@tomtom-org/maps-sdk/map';
-import { geocode, geometryData, search } from '@tomtom-org/maps-sdk/services';
+import { geocodeOne, geometryData, search } from '@tomtom-org/maps-sdk/services';
 import { bboxPolygon, difference } from '@turf/turf';
 import './style.css';
-import { LngLatBoundsLike } from 'maplibre-gl';
 import { API_KEY } from './config';
 
 // (Set your own API key when working in your own environment)
 TomTomConfig.instance.put({ apiKey: API_KEY });
 
 (async () => {
-    const placeToSearchInside = await geocode({ query: 'Amsterdam, NL', limit: 1 });
+    const placeToSearchInside = await geocodeOne('Amsterdam, NL');
     const fitBoundsOptions = { padding: 50 };
-    const bounds = placeToSearchInside.bbox as LngLatBoundsLike;
+    const bounds = placeToSearchInside.bbox as BBox;
 
     const map = new TomTomMap({
         container: 'sdk-map',
@@ -24,16 +23,18 @@ TomTomConfig.instance.put({ apiKey: API_KEY });
         .querySelector('#sdk-example-reCenter')
         ?.addEventListener('click', () => map.mapLibreMap.fitBounds(bounds, fitBoundsOptions));
 
-    const geometryToSearch = await geometryData({ geometries: placeToSearchInside });
+    const geometryToSearch = await geometryData({ geometries: [placeToSearchInside] });
+    const geometryFeature = geometryToSearch.features[0];
+
     // inverting for better look on the map
     const invertedGeometry = difference({
         type: 'FeatureCollection',
-        features: [bboxPolygon([-180, 90, 180, -90]), geometryToSearch?.features?.[0]],
-    });
+        features: [bboxPolygon([-180, 90, 180, -90]), geometryFeature],
+        bbox: geometryFeature.bbox,
+    }) as PolygonFeature;
+
     const geometriesModule = await GeometriesModule.get(map);
-    geometriesModule.show(
-        invertedGeometry ? { type: 'FeatureCollection', features: [invertedGeometry] } : geometryToSearch,
-    );
+    geometriesModule.show({ type: 'FeatureCollection', features: [invertedGeometry] });
     const placesInsideGeometry = await search({
         query: 'metro stop',
         geometries: [geometryToSearch],
