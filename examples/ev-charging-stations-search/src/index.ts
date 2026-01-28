1f78eb1cfa7747f4255680d1afe7dc7dbab6f21c
AlvaroGraca-TomTom <alvaro.graca@tomtom.com>
2026-01-28 15:33:51 +0100

    chore: rebase dealing with conflicts
    GitOrigin-RevId: 9f4f1eae42ba6f055e780cd1dd1c3dff7dc886db

--- examples/ev-charging-stations-search/src/index.ts
+++ examples/ev-charging-stations-search/src/index.ts
@@ -5,11 +5,12 @@ import {
     type PolygonFeatures,
     TomTomConfig,
 } from '@tomtom-org/maps-sdk/core';
-import { GeometriesModule, PlacesModule, POIsModule, TomTomMap } from '@tomtom-org/maps-sdk/map';
+import { GeometriesModule, PlacesModule, PlacesModuleConfig, POIsModule, TomTomMap } from '@tomtom-org/maps-sdk/map';
 import {
     geometryData,
     getPlacesWithEVAvailability,
     getPlaceWithEVAvailability,
+    hasChargingAvailability,
     search,
 } from '@tomtom-org/maps-sdk/services';
 import { bboxPolygon, difference } from '@turf/turf';
