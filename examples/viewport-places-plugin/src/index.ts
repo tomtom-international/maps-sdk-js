4e945a54640144ee26fd9e4259c99507d5f3d334
Daniel Fornies <daniel.forniessoria@tomtom.com>
2026-01-30 14:24:38 +0100

    fix: viewport places plugin example
    GitOrigin-RevId: 836abdd73e6fc4bb4829c7bb725521e149bf1db3

--- examples/viewport-places-plugin/src/index.ts
+++ examples/viewport-places-plugin/src/index.ts
@@ -7,22 +7,24 @@ import { API_KEY } from './config';
 // (Set your own API key when working in your own environment)
 TomTomConfig.instance.put({ apiKey: API_KEY, language: 'en-GB' });
 
-const map = new TomTomMap({
-    mapLibre: { container: 'sdk-map', center: [4.9041, 52.3676], zoom: 14 },
-});
+(async () => {
+    const map = new TomTomMap({
+        mapLibre: { container: 'sdk-map', center: [4.9041, 52.3676], zoom: 14 },
+    });
 
-const placesLayers = new ViewportPlaces(map);
-await placesLayers.addPOICategories({
-    minZoom: 12,
-    categories: ['PARKING_GARAGE', 'OPEN_PARKING_AREA', 'ELECTRIC_VEHICLE_STATION'],
-});
-// These categories will show up on top of (higher priority) the previous ones:
-await placesLayers.addPOICategories({
-    minZoom: 10,
-    categories: ['SUPERMARKETS_HYPERMARKETS'],
-});
-await placesLayers.add({
-    minZoom: 8,
-    searchOptions: { query: 'Lidl', poiCategories: ['SUPERMARKETS_HYPERMARKETS'], limit: 20 },
-    placesModuleConfig: { theme: 'pin' },
-});
+    const placesLayers = new ViewportPlaces(map);
+    await placesLayers.addPOICategories({
+        minZoom: 12,
+        categories: ['PARKING_GARAGE', 'OPEN_PARKING_AREA', 'ELECTRIC_VEHICLE_STATION'],
+    });
+    // These categories will show up on top of (higher priority) the previous ones:
+    await placesLayers.addPOICategories({
+        minZoom: 10,
+        categories: ['SUPERMARKETS_HYPERMARKETS'],
+    });
+    await placesLayers.add({
+        minZoom: 8,
+        searchOptions: { query: 'Lidl', poiCategories: ['SUPERMARKETS_HYPERMARKETS'], limit: 20 },
+        placesModuleConfig: { theme: 'pin' },
+    });
+})();
